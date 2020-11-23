33344193b3409db1165d75b841096a6331e3b631
Julienm25 <julienmauvais@hotmail.fr>
2020-11-23 15:26:15 +0100

    Problem merge

--- src/main/java/fr/utbm/ecole/core/service/ClientService.java
+++ src/main/java/fr/utbm/ecole/core/service/ClientService.java
@@ -1,6 +1,4 @@
 package fr.utbm.ecole.core.service;
-
-<<<<<<< HEAD
 import fr.utbm.ecole.core.entity.Course;
 import fr.utbm.ecole.core.repository.ConsoleCourseDao;
 import fr.utbm.ecole.core.repository.EntityCourseDao;
@@ -22,25 +20,6 @@ public class ClientService {
         List<Course> list = efd.listCourse();
         for (Course Course : list) {
             cfd.save(Course);
-=======
-import fr.utbm.ecole.core.entity.Client;
-import fr.utbm.ecole.core.repository.ConsoleClientDao;
-import fr.utbm.ecole.core.repository.EntityClientDao;
-import java.util.List;
-
-
-public class ClientService {
-     public void registerClient(Client c) {
-        ConsoleClientDao cfd = new ConsoleClientDao();
-        EntityClientDao efd = new EntityClientDao();
-
-        cfd.save(c);
-     
-        efd.save(c);
-        List<Client> list = efd.listClient();
-        for (Client Client : list) {
-            cfd.save(Client);
->>>>>>> a026e7a7846ac77019bca89bbd5a187f872f9a37
         }
        
     }
